031415e9ab55a0310042142df23767122c848634
st0rm348 <brunommsoares@outlook.com>
2024-08-27 23:49:58 +0100

    feat(admin): create dashboard card component based on shadcn components

---
+++ src/components/Dashboard-card.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+
+type DashboardCardProps = {
+  title: string,
+  subtitle: string,
+  body: string
+}
+const DashboardCard = ({ title, subtitle, body }: DashboardCardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  )
+}
+export default DashboardCard
