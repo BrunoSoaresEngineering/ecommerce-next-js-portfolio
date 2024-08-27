bb850ba15d35af69e553d865e84c7d781ccd5f8f
st0rm348 <brunommsoares@outlook.com>
2024-08-27 23:50:56 +0100

    feat(admin): add formatters lib

---
+++ src/lib/formatters.ts
@@ -0,0 +1,20 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat('pt-PT', {
+  currency: 'EUR',
+  style: 'currency',
+  minimumFractionDigits: 0
+})
+
+function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat('pt-PT');
+
+function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
+
+export {
+  formatCurrency,
+  formatNumber
+};
