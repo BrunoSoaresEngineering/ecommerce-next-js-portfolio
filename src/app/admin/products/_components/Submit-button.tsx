8d052382d869433f2e8ef1dcd698ca36450508c4
st0rm348 <brunommsoares@outlook.com>
2024-09-01 10:54:24 +0100

    feat(admin): add form submit button component

---
+++ src/app/admin/products/_components/Submit-button.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { useFormStatus } from 'react-dom';
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+    >
+      {pending ? 'Saving...' : 'Save'}
+    </Button>
+  );
+}
+export default SubmitButton;
