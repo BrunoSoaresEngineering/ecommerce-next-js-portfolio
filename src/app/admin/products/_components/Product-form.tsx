c9c776d85df8c6bacd81076112e91bdc0c732d5e
st0rm348 <brunommsoares@outlook.com>
2024-09-02 00:36:28 +0100

    feat(admin): adapt admin product form component to be used for editing

--- src/app/admin/products/_components/Product-form.tsx
+++ src/app/admin/products/_components/Product-form.tsx
@@ -2,19 +2,25 @@
 
 import { useState } from 'react';
 import { useFormState } from 'react-dom';
+import Image from 'next/image';
+import { Product } from '@prisma/client';
 import { formatCurrency } from '@/lib/formatters';
 import { Textarea } from '@/components/ui/textarea';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import Image from 'next/image';
-import { addProduct } from '../../_actions/products';
+import { addProduct, updateProduct } from '../../_actions/products';
 import SubmitButton from './Submit-button';
 
-type Props = {}
+type Props = {
+  product?: Product | null
+}
 
-function ProductForm({ props }: Props) {
-  const [errors, formAction] = useFormState(addProduct, { name: '' });
-  const [priceInCents, setPriceInCents] = useState(0);
+function ProductForm({ product = null }: Props) {
+  const [errors, formAction] = useFormState(
+    product ? updateProduct.bind(null, product.id) : addProduct,
+    { },
+  );
+  const [priceInCents, setPriceInCents] = useState(product?.priceInCents);
 
   return (
     <form action={formAction} className="space-y-8">
@@ -24,6 +30,7 @@ function ProductForm({ props }: Props) {
           type="text"
           id="name"
           name="name"
+          defaultValue={product?.name || ''}
           required
         />
         {errors?.name && <div className="text-destructive">{errors.name}</div>}
@@ -40,7 +47,7 @@ function ProductForm({ props }: Props) {
           onChange={(ev) => setPriceInCents(Number(ev.target.value))}
         />
         <div className="text-muted-foreground">
-          {formatCurrency((priceInCents / 100))}
+          {formatCurrency(((priceInCents || 0) / 100))}
         </div>
         {errors?.priceInCents && <div className="text-destructive">{errors.priceInCents}</div>}
       </div>
@@ -50,6 +57,7 @@ function ProductForm({ props }: Props) {
         <Textarea
           id="description"
           name="description"
+          defaultValue={product?.description || ''}
           required
         />
         {errors?.description && <div className="text-destructive">{errors.description}</div>}
@@ -61,8 +69,9 @@ function ProductForm({ props }: Props) {
           type="file"
           id="file"
           name="file"
-          required
+          required={product == null}
         />
+        {product && <div className="text-muted-foreground">{product.filePath}</div>}
         {errors?.file && <div className="text-destructive">{errors.file}</div>}
       </div>
 
@@ -72,8 +81,16 @@ function ProductForm({ props }: Props) {
           type="file"
           id="image"
           name="image"
-          required
+          required={product == null}
         />
+        {product && (
+          <Image
+            src={product.imagePath}
+            height="400"
+            width="400"
+            alt="Product Image"
+          />
+        )}
         {errors?.image && <div className="text-destructive">{errors.image}</div>}
       </div>
 
