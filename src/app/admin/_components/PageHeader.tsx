6f6368de032c7e860774201fa669a16cc2fa63d3
st0rm348 <brunommsoares@outlook.com>
2024-09-03 23:18:29 +0100

    style(admin): fix ESLint on admin header component

--- src/app/admin/_components/PageHeader.tsx
+++ src/app/admin/_components/PageHeader.tsx
@@ -4,9 +4,10 @@ type PageHeaderProps = {
   children: ReactNode
 };
 
-const PageHeader = ({ children }: PageHeaderProps) => {
+function PageHeader({ children }: PageHeaderProps) {
   return (
-    <h1 className='text-4xl mb-4'>{children}</h1>
-  )
+    <h1 className="text-4xl mb-4">{children}</h1>
+  );
 }
-export default PageHeader
+
+export default PageHeader;
