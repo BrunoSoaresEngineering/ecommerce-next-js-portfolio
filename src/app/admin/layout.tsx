158767a9bf409ddd60e84c1911e0f875d13c1b29
st0rm348 <brunommsoares@outlook.com>
2024-09-03 23:17:54 +0100

    style(admin): fix ESLint on admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
-import { Nav, NavLink } from '@/components/Nav'
-import { ReactNode } from 'react'
+import { Nav, NavLink } from '@/components/Nav';
+import { ReactNode } from 'react';
 
 export const dynamic = 'force-dynamic';
 
@@ -7,17 +7,18 @@ type AdminLayoutProps = {
   children: ReactNode
 }
 
-const adminLayout = ({ children }: Readonly<AdminLayoutProps>) => {
+function adminLayout({ children }: Readonly<AdminLayoutProps>) {
   return (
     <>
       <Nav>
-        <NavLink href='/admin'>Dashboard</NavLink>
-        <NavLink href='/admin/products'>Products</NavLink>
-        <NavLink href='/admin/users'>Customers</NavLink>
-        <NavLink href='/admin/orders'>Sales</NavLink>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
       </Nav>
-      <div className='container my-6'>{children}</div>
+      <div className="container my-6">{children}</div>
     </>
-  )
+  );
 }
+
 export default adminLayout;
