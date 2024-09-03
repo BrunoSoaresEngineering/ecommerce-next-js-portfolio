99526ca1281242471392986204258181539475b5
st0rm348 <brunommsoares@outlook.com>
2024-09-03 23:21:20 +0100

    style(admin): fix ESLint on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,12 +5,12 @@ import { formatNumber, formatCurrency } from '@/lib/formatters';
 async function getSalesData() {
   const data = await db.order.aggregate({
     _sum: { pricePaidInCents: true },
-    _count: true
+    _count: true,
   });
 
   return {
     amount: (data._sum.pricePaidInCents || 0) / 100,
-    numberOfSales: data._count
+    numberOfSales: data._count,
   };
 }
 
@@ -18,60 +18,60 @@ async function getUserData() {
   const [userCount, orderData] = await Promise.all([
     db.user.count(),
     db.order.aggregate({
-      _sum: { pricePaidInCents: true }
-    })
+      _sum: { pricePaidInCents: true },
+    }),
   ]);
 
-  const averageValuePerUser = userCount === 0 
-    ? 0 
-    : (orderData.__sum.pricePaidInCents || 0) / userCount / 100;
+  const averageValuePerUser = userCount === 0
+    ? 0
+    : (orderData._sum.pricePaidInCents || 0) / userCount / 100;
 
   return {
     userCount,
-    averageValuePerUser
-  }
+    averageValuePerUser,
+  };
 }
 
 async function getProductData() {
   const [activeCount, inactiveCount] = await Promise.all([
-    db.product.count({ where: { isAvailableForPurchase: true }}),
-    db.product.count({ where: { isAvailableForPurchase: false }})
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
   ]);
 
   return {
     activeCount,
-    inactiveCount
+    inactiveCount,
   };
 }
 
-type Props = {}
-const AdminDashboard = async (props: Props) => {
+async function AdminDashboard() {
   const [salesData, userData, productData] = await Promise.all([
     getSalesData(),
     getUserData(),
-    getProductData()
+    getProductData(),
   ]);
 
   return (
-    <div className='grid grid-cols-1 md:grid-flow-cols-2 lg:grid-cols-3 gap-4'>
+    <div className="grid grid-cols-1 md:grid-flow-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
-        title='Sales'
+        title="Sales"
         subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
         body={`${formatCurrency(salesData.amount)}`}
       />
 
       <DashboardCard
-        title='Customers'
+        title="Customers"
         subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`}
         body={`${formatNumber(userData.userCount)}`}
       />
 
       <DashboardCard
-        title='Active Products'
+        title="Active Products"
         subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
         body={`${formatNumber(productData.activeCount)}`}
       />
     </div>
-  )
+  );
 }
+
 export default AdminDashboard;
