2239a24b5bce351795405fb2c8a4dd5403f40592
st0rm348 <brunommsoares@outlook.com>
2024-09-05 23:45:24 +0100

    feat(home): add product card component

---
+++ src/components/Product-card.tsx
@@ -0,0 +1,46 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { formatCurrency } from '@/lib/formatters';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from './ui/button';
+
+type Props = {
+  id: string,
+  name: string,
+  priceInCents: number,
+  description: string,
+  imagePath: string,
+}
+
+function ProductCard({
+  id, name, priceInCents, description, imagePath,
+}: Props) {
+  return (
+    <Card className="flex flex-col overflow-hidden">
+      <div className="relative w-full h-auto aspect-video">
+        <Image src={imagePath} alt={name} fill />
+      </div>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-grow">
+        <p className="line-clamp-4">{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Button asChild size="lg" className="w-full">
+          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProductCard;
