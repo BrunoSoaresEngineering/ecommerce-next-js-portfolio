89b95edb5f593fded66858b50a3341f3c3e4f83d
st0rm348 <brunommsoares@outlook.com>
2024-09-09 21:09:45 +0100

    feat(products): add and configure grid on the page

--- src/app/(customer-facing)/products/page.tsx
+++ src/app/(customer-facing)/products/page.tsx
@@ -39,7 +39,7 @@ async function ProductsSuspense() {
 
 async function ProductsPage() {
   return (
-    <main>
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <Suspense fallback={<Loading />}>
         <ProductsSuspense />
       </Suspense>
