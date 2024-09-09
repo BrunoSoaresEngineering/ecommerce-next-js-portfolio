09d24f69e49a8604acd79b22576e579bb7ad6e85
st0rm348 <brunommsoares@outlook.com>
2024-09-09 19:57:15 +0100

    feat(products): add suspense logic while loading products

--- src/app/(customer-facing)/products/page.tsx
+++ src/app/(customer-facing)/products/page.tsx
@@ -1,26 +1,48 @@
-import { ProductCard } from '@/components/Product-card';
+import { ProductCard, ProductCardSkeleton } from '@/components/Product-card';
 import db from '@/db/db';
+import { Suspense } from 'react';
 
-const getProducts = () => db.product.findMany({
-  where: { isAvailableForPurchase: true },
-  orderBy: { name: 'asc' },
-});
+async function getProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { name: 'asc' },
+  });
+}
 
-async function ProductsPage() {
+function Loading() {
+  return (
+    <>
+      <ProductCardSkeleton />
+      <ProductCardSkeleton />
+      <ProductCardSkeleton />
+      <ProductCardSkeleton />
+      <ProductCardSkeleton />
+      <ProductCardSkeleton />
+    </>
+  );
+}
+
+async function ProductsSuspense() {
   const products = await getProducts();
 
+  return products.map((product) => (
+    <ProductCard
+      key={product.id}
+      name={product.name}
+      description={product.description}
+      id={product.id}
+      imagePath={product.imagePath}
+      priceInCents={product.priceInCents}
+    />
+  ));
+}
+
+async function ProductsPage() {
   return (
     <main>
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          name={product.name}
-          description={product.description}
-          id={product.id}
-          imagePath={product.imagePath}
-          priceInCents={product.priceInCents}
-        />
-      ))}
+      <Suspense fallback={<Loading />}>
+        <ProductsSuspense />
+      </Suspense>
     </main>
   );
 }
