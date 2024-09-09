70963a3f88a5a0f2263b45804a5eb490baf825eb
st0rm348 <brunommsoares@outlook.com>
2024-09-09 18:06:25 +0100

    feat(home): add product skeleton component

--- src/components/Product-card.tsx
+++ src/components/Product-card.tsx
@@ -43,4 +43,31 @@ function ProductCard({
   );
 }
 
-export default ProductCard;
+function ProductCardSkeleton() {
+  return (
+    <Card className="flex flex-col overflow-hidden animate-pulse">
+      <div className="relative w-full aspect-video bg-gray-300" />
+      <CardHeader>
+        <CardTitle>
+          <div className="w-3/4 h-6 rounded-full bg-gray-300" />
+        </CardTitle>
+        <CardDescription>
+          <div className="w-1/2 h-4 rounded-full bg-gray-300" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="w-full h-4 rounded-full bg-gray-300" />
+        <div className="w-full h-4 rounded-full bg-gray-300" />
+        <div className="w-3/4 h-4 rounded-full bg-gray-300" />
+      </CardContent>
+      <CardFooter>
+        <Button size="lg" className="w-full" />
+      </CardFooter>
+    </Card>
+  );
+}
+
+export {
+  ProductCard,
+  ProductCardSkeleton,
+};
