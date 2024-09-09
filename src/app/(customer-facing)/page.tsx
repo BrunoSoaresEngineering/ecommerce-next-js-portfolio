3b8ce6a8f78a99db2aa3c6aa9e072e0a1f1a2f82
st0rm348 <brunommsoares@outlook.com>
2024-09-10 00:33:17 +0100

    feat(home): add memoization and data caches

--- src/app/(customer-facing)/page.tsx
+++ src/app/(customer-facing)/page.tsx
@@ -1,11 +1,19 @@
 import ProductSection from '@/components/Product-section';
 import { getMostPopularProducts, getNewestProducts } from '@/lib/product';
+import { cache } from 'react';
+
+const DAY_IN_SECONDS = 60 * 60 * 24;
+
+export const revalidate = DAY_IN_SECONDS;
+
+const getMostPopularProductsMemoized = cache(() => getMostPopularProducts(6));
+const getNewestProductsMemoized = cache(() => getNewestProducts(6));
 
 export default async function Home() {
   return (
     <main className="space-y-12">
-      <ProductSection title="Most Popular" productsFetcher={() => getMostPopularProducts(6)} />
-      <ProductSection title="Newest" productsFetcher={() => getNewestProducts(6)} />
+      <ProductSection title="Most Popular" productsFetcher={getMostPopularProductsMemoized} />
+      <ProductSection title="Newest" productsFetcher={getNewestProductsMemoized} />
     </main>
   );
 }
