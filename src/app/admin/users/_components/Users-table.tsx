77321f30c9d9910c2c461842ccfc188f39e2974e
st0rm348 <brunommsoares@outlook.com>
2024-09-13 21:57:50 +0100

    feat(admin): add users table admin component

---
+++ src/app/admin/users/_components/Users-table.tsx
@@ -0,0 +1,64 @@
+import { Order } from '@prisma/client';
+import {
+  Table, TableBody, TableCell, TableHead, TableHeader, TableRow,
+} from '@/components/ui/table';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import db from '@/db/db';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
+import { MoreVertical } from 'lucide-react';
+import DeleteDropdownItem from './User-actions';
+
+function sumOrders(orders: Pick<Order, 'pricePaidInCents'>[]) {
+  const sum = orders.reduce((grandTotal, order) => grandTotal + order.pricePaidInCents, 0);
+
+  return sum / 100;
+}
+
+async function UsersTable() {
+  const users = await db.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      orders: { select: { pricePaidInCents: true } },
+    },
+    orderBy: { createdAt: 'desc' },
+  });
+
+  if (users.length === 0) {
+    return <p>No Customers Found</p>;
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead>Value</TableHead>
+          <TableHead className="sr-only">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{formatNumber(user.orders.length)}</TableCell>
+            <TableCell>{formatCurrency(sumOrders(user.orders))}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DeleteDropdownItem id={user.id} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+export default UsersTable;
