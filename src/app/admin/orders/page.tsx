5140a4dcd091950da53133a69f1f3d023fa0795d
st0rm348 <brunommsoares@outlook.com>
2024-09-14 00:15:47 +0100

    feat(admin): add admin orders page

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,12 @@
+import PageHeader from '../_components/PageHeader';
+import SalesTable from './_components/Sales-table';
+
+function OrdersPage() {
+  return (
+    <>
+      <PageHeader>Sales</PageHeader>
+      <SalesTable />
+    </>
+  );
+}
+export default OrdersPage;
