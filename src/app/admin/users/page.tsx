d673094d1f8fb83849e4d696a9256dda9b0fffff
st0rm348 <brunommsoares@outlook.com>
2024-09-13 21:58:23 +0100

    feat(admin): add admin users page

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,12 @@
+import PageHeader from '../_components/PageHeader';
+import UsersTable from './_components/Users-table';
+
+function UsersPage() {
+  return (
+    <>
+      <PageHeader>Customers</PageHeader>
+      <UsersTable />
+    </>
+  );
+}
+export default UsersPage;
